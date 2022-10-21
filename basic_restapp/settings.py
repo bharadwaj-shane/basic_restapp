a1101ec1ff8c8f32ae56f8823f01eb772ced059a
Shantanu Dutta <bharadwaj.shane@gmail.com>
2022-10-21 17:35:47 +0530

    update

--- basic_restapp/settings.py
+++ basic_restapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-^8xs(mc_e35t*tu(tu8hkf7*0y29bnbngx#h3e5grsus2ozau8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "app"
 ]
 
 MIDDLEWARE = [
