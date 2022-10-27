cb39e62af315b75b1ff4cafce819d8ac14780b08
Shantanu Dutta <bharadwaj.shane@gmail.com>
2022-10-27 18:53:45 +0530

    update view
    update

--- app/views.py
+++ app/views.py
@@ -9,32 +9,40 @@ import numpy as np
 @api_view(['GET'])
 def calcnormal(request, format=None):
     """API to process long running application"""
-    result = multi(10000,10000,1000)
-    return Response(result, status=status.HTTP_201_CREATED)
+    result = multi(10000, 10000, 10000, 10000)
+    if result["Result"] != None :
+        return Response(result, status=status.HTTP_201_CREATED)
+    else :
+        return Response(result, status=status.HTTP_400_BAD_REQUEST)
+    
 
 @api_view(['POST'])
 def calculate(request, format=None):
     """API to process long running application"""
-    # data = json.load(request.data)
-    # print("\val1:", data['k1'])
-    # print("\val2:", data['k2'])
-    result = multi(request.data['row1'],request.data['row2'],request.data['col'])
+    result = multi(request.data['row1'],request.data['col1'],request.data['row2'],request.data['col2'])
     return Response(result, status=status.HTTP_201_CREATED)
 
 
-def multi(row1, row2, col):
-    res_dict = {"Result": []}
+def multi(row1, col1, row2, col2):
+    if col1 != row2 :
+        return {"Result": None}
+    
+    res_val = {}
 
-    random_data1 = np.random.rand(row1, col)
-    random_data2 = np.random.rand(row2, col)
+    random_data1 = np.random.rand(row1, col1)
+    random_data2 = np.random.rand(row2, col2)
 
     df1 = pd.DataFrame(random_data1)
     df2 = pd.DataFrame(random_data2)
 
-    result = df1.mul(df2)
+    result = df1.dot(df2)
     df_mean = result.mean()
-    lst = df_mean.tolist()
-    res_dict["Result"] = lst
-    sleep(20)
+    df_std = result.std()
+    lst_mean = df_mean.tolist()
+    lst_std = df_std.tolist()
+    res_val["Mean"] = lst_mean
+    res_val["Std. Dev"] = lst_std
+    # sleep(20)
+    tot_res = {"Result":res_val}
     
-    return res_dict
+    return tot_res
